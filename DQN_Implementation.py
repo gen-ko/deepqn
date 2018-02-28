d81d394e09e5fd5b2d1f94f35adabdd6d60339ba
Yuan Liu <yuanl5@andrew.cmu.edu>
2018-02-28 16:29:16 -0500

    Cleaned some code

--- DQN_Implementation.py
+++ DQN_Implementation.py
@@ -8,15 +8,14 @@ class QNetwork():
     # This class essentially defines the network architecture.
     # The network should take in state of the world as an input,
     # and output Q values of the actions available to the agent as the output.
-
-    def __init__(self, env, batch_size=32):
+    def __init__(self, env, batch_size: int=32, trainable=True):
         # Define your network architecture here. It is also a good idea to define any training operations
         # and optimizers here, initialize your variables, or alternately compile your model here.
-        self.batch_size = batch_size
+        self.batch_size: int = batch_size
         if env.spec.id in SIMPLE_ENVS:
             # Initialize a linear function
             self.input = tf.placeholder(dtype=tf.float32,
-                                        shape=(self.batch_size, env.observation_space.shape),
+                                        shape=(self.batch_size, env.observation_space.shape[0]),
                                         name='input')
             self.output = tf.layers.dense(
                 inputs=self.input,
@@ -28,7 +27,7 @@ class QNetwork():
                 kernel_regularizer=None,
                 bias_regularizer=None,
                 activity_regularizer=None,
-                trainable=True,
+                trainable=trainable,
                 name='output',
                 reuse=None)
         else:
@@ -117,6 +116,7 @@ class QNetwork():
         pass
 
 
+
 class Replay_Memory():
 
     def __init__(self, memory_size=50000, burn_in=10000):
@@ -163,27 +163,33 @@ class DQN_Agent():
         self.gamma = gamma
         return
 
-    def epsilon_greedy_policy(self):
+    def epsilon_greedy_policy(self, q_values):
         # Creating epsilon greedy probabilities to sample from.
 
         # q_values is the node in tf graph where the Q-network outputs
         q_values = self.model.output
-
-
-        # decide to use deterministic or random
-        chose_random = tf.random_uniform(tf.stack([self.batch_size]),
-                                         minval=0,
-                                         maxval=1,
-                                         dtype=tf.float32) < self.eps
-
-        actions = tf.where(chose_random,
-                                      tf.random_uniform(tf.stack([self.batch_size]),
-                                                        minval=0,
-                                                        maxval=self.num_actions,
-                                                        dtype=tf.int32),
-                                      tf.argmax(q_values))
-
+        '''
+                # decide to use deterministic or random
+                chose_random = tf.random_uniform(tf.stack([self.batch_size]),
+                                                 minval=0,
+                                                 maxval=1,
+                                                 dtype=tf.float32) < self.eps
+        
+                actions = tf.where(chose_random,
+                                              tf.random_uniform(tf.stack([self.batch_size]),
+                                                                minval=0,
+                                                                maxval=self.num_actions,
+                                                                dtype=tf.int32),
+                                          tf.argmax(q_values))
+        '''
         # return this operator
+        actions = []
+        for i in range(self.batch_size):
+            if np.random.uniform(low=0.0, high=1.0) < self.eps:
+                actions.append(np.random.randint(0, self.num_actions))
+            else:
+                actions.append(np.argmax(q_values[i]))
+
         return actions
 
     def greedy_policy(self):
@@ -197,30 +203,58 @@ class DQN_Agent():
         # If training without experience replay_memory, then you will interact with the environment
         # in this function, while also updating your network parameters.
 
+        # give a batch of envs
+
         # reset a batch of envs
         envs = []
         for i in range(self.batch_size):
             envs.append(gym.make(self.env_name))
 
-        obs = []
+        # find the corresponding batch of current states
+        states_current = []
         for i in range(self.batch_size):
-            obs.append(envs[i].reset())
+            states_current.append(envs[i].reset())
 
-        action_node = self.epsilon_greedy_policy()
-        action_values = sess.run(action_node, feed_dict={self.model.input: obs})
-
+        # find the next states
+        states_next = []
         rewards = []
         for i in range(self.batch_size):
-            rewards.append(envs[i].step(action_values[i]))
+            states_next.append([])
+            rewards.append([])
+            for j in range(self.num_actions):
+                next_state, reward, is_terminal, info = envs[i].step[j]
+                states_next[i].append(next_state)
+                rewards[i].append(reward)
 
-        # target value
-        sess.run(self.model.output, feed_dict={self.model.input: obs})
+        states_next = np.array(states_next)
+        rewards = np.array(rewards)
 
-        # the placeholder for rewards
-        rewards_ph = tf.placeholder(dtype=tf.float32, shape=(self.batch_size, self.num_actions), name='rewards')
+        q_values_next = np.zeros(shape=[self.batch_size, self.num_actions])
+        for j in range(self.num_actions):
+            q_values_next[:, j] = sess.run(self.model.output, feed_dict={self.model.input: states_next[:, j]})
+
+        actions_next_best = np.argmax(q_values_next, axis=1)
+
+        rewards_next_best = rewards[:, actions_next_best]
 
 
-        pass
+        rewards_node = tf.placeholder(dtype=tf.float32, shape=(self.batch_size, 1))
+
+        q_values_node = self.model.output
+
+        q_values_next_node = tf.placeholder(dtype=tf.float32, shape=(self.batch_size, self.num_actions))
+
+        q_values_max_node = tf.reduce_max(q_values_next_node)
+
+        target_node = rewards_node + q_values_max_node
+
+        loss_node = tf.reduce_mean(tf.squared_difference(q_values_node, target_node))
+
+        train_op = tf.train.AdamOptimizer(0.001).minimize(loss_node)
+
+        sess.run(train_op, {rewards_node: rewards, q_values_next_node: q_values_next, self.model.input: states_current})
+
+        return
 
 
     def test(self, model_file=None):
@@ -245,21 +279,23 @@ def parse_arguments():
 def main(args):
 
     args = parse_arguments()
-    env = gym.make(args.env)
+    #env = gym.make(args.env)
 
+    env = gym.make('CartPole-v0')
 
     # Setting the session to allow growth, so it doesn't allocate all GPU memory.
     gpu_ops = tf.GPUOptions(allow_growth=True)
     config = tf.ConfigProto(gpu_options=gpu_ops)
     sess = tf.Session(config=config)
 
-    init = tf.global_variables_initializer()
-    sess.run(init)
+
 
     agent = DQN_Agent(env)
 
-    agent.train(sess)
-    agent.test(model_file='model.mdf')
+    init = tf.global_variables_initializer()
+    sess.run(init)
+
+    agent.train_online(sess)
 
     # Setting this as the default tensorflow session.
     keras.backend.tensorflow_backend.set_session(sess)
