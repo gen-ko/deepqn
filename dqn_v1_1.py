7e8d71f8cd585dd04acdebafe3e82c4ce2527166
Yuan Liu <yuanl5@andrew.cmu.edu>
2018-03-02 09:16:30 -0500

    --

--- dqn_v1_1.py
+++ dqn_v1_1.py
@@ -4,33 +4,117 @@ import tensorflow as tf
 import numpy as np
 import gym, sys, copy, argparse
 
+class Agent(object):
+    def __init__(self):
+        self.env = gym.make('CartPole-v0')
 
 
+        # Setting the session to allow growth, so it doesn't allocate all GPU memory.
+        gpu_ops = tf.GPUOptions(allow_growth=True)
+        config = tf.ConfigProto(gpu_options=gpu_ops)
+        self.sess = tf.Session(config=config)
+
+        self.num_actions = self.env.action_space.n
+
+        # the dimension of a state observation
+        self.state_dim = self.env.observation_space.shape[0]
+
+        self.learning_rate = 0.0001
+
+        self.gamma = 0.99
+
+        self.eps = 0.05
+
+        self.iterations: int = 1000000
+
+        self.save_path = "./tmp/model_dqn_v1_1.ckpt"
+
+        # instantiate the DQN
+        self.dqn = DQN_v1(self.state_dim, self.num_actions, self.learning_rate)
+
+        # initialize tf graph
+        init = tf.global_variables_initializer()
+        self.sess.run(init)
+        return
+
+    def epsilon_greedy_policy(self, q):
+        # Creating epsilon greedy probabilities to sample from.
+        if np.random.uniform(low=0.0, high=1.0) < self.eps:
+            action = np.random.randint(0, self.num_actions)
+        else:
+            action = np.argmax(q)
+        return action
+
+    def greedy_policy(self, q):
+        action = np.argmax(q)
+        return action
+
+    def train(self):
+
+        episode_num: int = 0
+        cumulative_reward = 0.0
+        avg_reward = 0.0
+        cumulative_episode: int = 0
+
+        state = self.env.reset()
+
+        for iter_i in range(self.iterations):
+
+            q = self.sess.run(fetches=self.dqn.q_tensor,
+                              feed_dict={self.dqn.state_ph: state})
+
+            action = self.epsilon_greedy_policy(q)
+
+            ######
+
+            state_next, reward, is_terminal, _ = self.env.step(action)
+
+            cumulative_reward = cumulative_reward + reward
+
+            q_next = self.sess.run(fetches=self.dqn.q_tensor,
+                                   feed_dict={self.dqn.state_ph: state_next})
+
+            # optimal action
+            q_next_max = max(max(q_next))
+
+            target = self.gamma * q_next_max + reward
+
+            self.sess.run(fetches=self.dqn.train_op,
+                          feed_dict={self.dqn.target_ph: target,
+                                     self.dqn.state_ph: state,
+                                     self.dqn.action_ph: action})
+
+            # prepare the next loop
+            state = state_next
+
+            if is_terminal:
+                episode_num += 1
+                state = self.env.reset()
+                cumulative_episode += 1
+
+                avg_reward += cumulative_reward
+                cumulative_reward = 0.0
+
+                if cumulative_episode % 10 == 9:
+                    print('recent 10 episode average award: ', avg_reward / 10)
+                    avg_reward = 0.0
+
+            # loop_counter += 1
+            if iter_i % 10000 == 9999:
+                print('iterations passed: ', iter_i + 1)
+
+        print('Training done')
+
+        self.save_model()
+
+        return
+
+    def save_model(self):
+        saver = tf.train.Saver()
+        save_path = saver.save(self.sess, self.save_path)
+        print("Model saved in path: %s" % save_path)
 
 
-def epsilon_greedy_policy(q, num_actions):
-    # Creating epsilon greedy probabilities to sample from.
-
-    eps = 0.05
-
-    if np.random.uniform(low=0.0, high=1.0) < eps:
-        actions = np.random.randint(0, num_actions)
-    else:
-        actions = np.argmax(q)
-
-    return actions
-
-
-def greedy_policy(q, num_actions, batch_size=1):
-    if batch_size == 1:
-        # actions shape (1, )
-        actions = np.argmax(q)
-    else:
-        # actions shape (batch_size, 1)
-        actions = np.zeros((batch_size, ), dtype=int)
-        for i in range(batch_size):
-            actions[i] = np.argmax(q[i])
-    return actions
 
 
 class DQN_v1(object):
@@ -42,13 +126,14 @@ class DQN_v1(object):
         self.tensor1 = tf.reshape(self.state_ph, shape=(1, state_dim))
 
         self.tensor2 = tf.layers.dense(inputs=self.tensor1,
-                                       units=30,
-                                       activation=tf.nn.relu,
-                                       use_bias=True,
-                                       kernel_initializer=tf.random_normal_initializer(),
-                                       bias_initializer=tf.zeros_initializer(),
-                                       trainable=True,
-                                       reuse=None)
+                                        units=8,
+                                        activation=tf.nn.relu,
+                                        use_bias=True,
+                                        kernel_initializer=tf.random_normal_initializer(),
+                                        bias_initializer=tf.zeros_initializer(),
+                                        name='t2',
+                                        trainable=True,
+                                        reuse=None)
 
         self.q_tensor = tf.layers.dense(inputs=self.tensor2,
                                         units=num_actions,
@@ -56,10 +141,9 @@ class DQN_v1(object):
                                         use_bias=True,
                                         kernel_initializer=tf.random_normal_initializer(),
                                         bias_initializer=tf.zeros_initializer(),
+                                        name='q',
                                         trainable=True,
                                         reuse=None)
-
-        self.q_flat_tensor = tf.reshape(self.q_tensor, shape=[num_actions])
 
         self.target_ph = tf.placeholder(dtype=tf.float32,
                                         shape=None,
@@ -69,7 +153,7 @@ class DQN_v1(object):
                                         shape=None,
                                         name='action_ph')
 
-        self.q_selected_tensor = tf.gather(self.q_flat_tensor, self.action_ph, axis=0, name='q_selected')
+        self.q_selected_tensor = tf.gather(self.q_tensor, self.action_ph, axis=1, name='q_selected')
 
         self.loss = tf.reduce_mean(tf.squared_difference(self.target_ph, self.q_selected_tensor), name='loss')
 
@@ -79,107 +163,7 @@ class DQN_v1(object):
         return
 
 
-def train():
-    env = gym.make('CartPole-v0')
 
-    # Setting the session to allow growth, so it doesn't allocate all GPU memory.
-    gpu_ops = tf.GPUOptions(allow_growth=True)
-    config = tf.ConfigProto(gpu_options=gpu_ops)
-    sess = tf.Session(config=config)
-
-    num_actions = env.action_space.n
-
-    # the dimension of a state observation
-    state_dim = env.observation_space.shape[0]
-
-    batch_size = 1
-
-    update_steps: int = 10
-
-    learning_rate = 0.0001
-
-    gamma = 0.99
-
-    iterations: int = 20000
-
-    # instantiate the DQN
-    dqn = DQN_v1(state_dim, num_actions, learning_rate)
-
-    state = env.reset()
-
-    # initialize tf graph
-    init = tf.global_variables_initializer()
-    sess.run(init)
-
-    episode_num: int = 0
-
-    cumulative_reward = 0.0
-    avg_reward = 0.0
-    cumulative_episode: int= 0
-    minor_step: int = 0
-
-
-    for iter_i in range(iterations):
-
-
-        q = sess.run(fetches=dqn.q_tensor,
-                     feed_dict={dqn.state_ph: state})
-
-        action = epsilon_greedy_policy(q, num_actions)
-
-        ######
-
-        state_next, reward, is_terminal, _ = env.step(action)
-
-        cumulative_reward = cumulative_reward + reward
-
-
-        q_next = sess.run(fetches=dqn.q_tensor,
-                          feed_dict={dqn.state_ph: state_next})
-
-        # optimal action
-        q_next_max = max(max(q_next))
-
-        target = gamma * q_next_max + reward
-
-        sess.run(fetches=dqn.train_op,
-                 feed_dict={dqn.target_ph: target,
-                            dqn.state_ph: state,
-                            dqn.action_ph: action})
-
-        # prepare the next loop
-        state = state_next
-
-        if is_terminal:
-            episode_num += 1
-            state = env.reset()
-            cumulative_episode += 1
-
-            avg_reward += cumulative_reward
-            cumulative_reward = 0.0
-
-            if cumulative_episode % 10 == 9:
-                print('recent 10 episode average award: ', avg_reward / 10)
-                avg_reward = 0.0
-
-        # loop_counter += 1
-        if iter_i % 10000 == 9999:
-            print('iterations passed: ', iter_i + 1)
-
-
-    print('Training done')
-    #print('discrepency: ', discrepency)
-
-    # save model
-    saver = tf.train.Saver()
-
-    # Later, launch the model, initialize the variables, do some work, and save the
-    # variables to disk.
-
-    save_path = saver.save(sess, "./tmp/model_dqn_v1_1.ckpt")
-    print("Model saved in path: %s" % save_path)
-
-    return
 
 
 def test(render=False):
@@ -210,7 +194,7 @@ def test(render=False):
     # load weights
     saver = tf.train.Saver()
 
-    saver.restore(sess, "./tmp/model_dqn_v1_1.ckpt")
+    saver.restore(sess, "./tmp/model_dqn_v1.ckpt")
 
     avg_episode_reward = 0.0
     for epi in range(test_episodes):
@@ -263,11 +247,11 @@ def test(render=False):
 
 def main():
     print(tf.__version__)
-    tf.set_random_seed(2022)
+    agent = Agent()
     is_train = True
     is_test = False
     if is_train:
-        train()
+        agent.train()
     if is_test:
         test(render=True)
     return
