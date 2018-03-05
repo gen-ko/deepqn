3329e0ce30f33a86ed5337cdcefba21b57ccd52e
Yuan Liu <yuanl5@andrew.cmu.edu>
2018-03-05 15:52:43 -0500

    Revert "delete redundant EnvWrapper in scripts"
    This reverts commit 8dbaba06792666f9ab5fcf5c32b84a3f273cee55.

--- script_v4.py
+++ script_v4.py
@@ -4,12 +4,58 @@ import tensorflow as tf
 import numpy as np
 import gym, sys, copy, argparse
 
+from image_preprocessing import image_prep
+
+
+from collections import deque
 
 from memory_replay import MemoryReplayerTF
 from deep_qn import DeepQN
 from tester import Tester
 
-from env_wrapper import EnvWrapper
+
+
+class EnvWrapper(object):
+    def __init__(self, env, frame_stack=1):
+        self.env = env
+        self.action_space = self.ActionSpace(env.action_space.n)
+
+        self.state_shape = (4, 84, 84)
+
+        # tmp_shape = (env.observation_space.shape, frame_stack)
+
+        # self.observation_space = self.ObservationSpace(tmp_shape)
+        self.frame_stack = frame_stack
+        # self.state_shape = tmp_shape
+
+        self.si = deque(iterable=[], maxlen=frame_stack)
+        self.render = self.env.render
+        return
+
+    def step(self, a):
+        si, r, done, info = self.env.step(a)
+        si = image_prep(si)
+        self.si.append(si)
+        s = np.array(self.si)
+        return s, r, done, info
+
+    def reset(self):
+        self.si.append(image_prep(self.env.reset()))
+        self.si.append(image_prep(self.env.reset()))
+        self.si.append(image_prep(self.env.reset()))
+        self.si.append(image_prep(self.env.reset()))
+        s = np.array(self.si)
+        return s
+
+    class ActionSpace(object):
+        def __init__(self, n):
+            self.n = n
+            return
+
+    class ObservationSpace(object):
+        def __init__(self, shape):
+            self.shape = shape
+            return
 
 def train():
     print(tf.__version__)
@@ -18,13 +64,25 @@ def train():
                             log_device_placement=True)
     sess = tf.Session(config=config)
 
-    env = EnvWrapper('SpaceInvaders-v0')
+    envi = gym.make('SpaceInvaders-v0')
 
+<<<<<<< HEAD
     mr = MemoryReplayerTF(state_shape=env.state_shape, capacity=100000)
 
     mr_s, mr_s_, mr_r, mr_a, mr_done = mr.replay_register()
+=======
+    num_actions = 6
 
-    qn = DeepQN(state_shape=env.state_shape, num_actions=env.num_actions, gamma=0.99, type='v4')
+    state_shape = (4, 84, 84)
+>>>>>>> parent of 8dbaba0... delete redundant EnvWrapper in scripts
+
+    env = EnvWrapper(envi, frame_stack=4)
+
+    env2 = gym.make('SpaceInvaders-v0')
+
+    mr = MemoryReplayer(state_shape=state_shape, capacity=1000)
+
+    qn = DeepQN(state_shape=state_shape, num_actions=num_actions, gamma=0.99, type='v4')
 
     loss = qn.loss_graph(mr_s, mr_s_, mr_r, mr_a, mr_done)
 
@@ -86,9 +144,7 @@ def test(render=False, path='./tmp/dqn_v4.ckpt', episodes=100):
     config = tf.ConfigProto(gpu_options=gpu_ops)
     sess = tf.Session(config=config)
 
-    env = EnvWrapper('SpaceInvaders-v0')
-
-    qn = DeepQN(state_shape=env.state_shape, num_actions=env.num_actions, gamma=0.99)
+    qn = DeepQN(state_shape=(84,84,4), num_actions=6, gamma=0.99)
 
     qn.reset_sess(sess)
 
