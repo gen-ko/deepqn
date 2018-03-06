c4e88ce64ef93ff0477bd5701c363c30216da8a4
hengruo <hengruoz@gmail.com>
2018-03-06 14:51:27 -0500

    modified:   env_wrapper.py

--- env_wrapper.py
+++ env_wrapper.py
@@ -9,7 +9,7 @@ class EnvWrapper(object):
     def __init__(self, args, mod_r=False, monitor=False):
         self.env_name = args.env
         if monitor:
-            self.env = wrappers.Monitor(gym.make(self.env_name), 'tmp/{}_{}_record'.format(args.env, args.qnum))
+            self.env = wrappers.Monitor(gym.make(self.env_name), 'tmp/{}_{}_record'.format(args.env, args.qnum), force=False)
         else:
             self.env = gym.make(self.env_name)
         self.num_actions = self.env.action_space.n
