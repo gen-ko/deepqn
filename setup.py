d5adb847af1192cd7eaf21141962713025e2e9c8
Yuan Liu <yuanl5@andrew.cmu.edu>
2018-03-29 09:12:46 -0400

    add somefiles to .gitignore

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='deepqn',
-      version='0.0.1',
+      version='0.0.2',
       description='A DQN implementation',
-      url='https://github.com/linthieda/dqn',
+      url='https://github.com/linthieda/deepqn',
       author='Yuan Liu',
       author_email='linthieda@gmail.com',
       license='MIT',
