54f9548bfec85b1f86c563c10ac2c771ec980bfb
Yuan Liu <yuanl5@andrew.cmu.edu>
2018-03-29 00:15:07 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dqn',
+      version='0.1',
+      description='DQN implementation',
+      url='https://github.com/linthieda/dqn',
+      author='Yuan Liu',
+      author_email='linthieda@gmail.com',
+      license='MIT',
+      packages=['dqn'],
+      zip_safe=False)
